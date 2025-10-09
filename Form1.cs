393873445edb98d9615998d4901b1b223bf30e19
24jn0118-Shi <24jn0118@jec.ac.jp>
2025-10-09 10:15:59 +0900

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlexOrder_Prototype
+{
+    
+    public partial class Form1 : Form
+    {
+        int cnt = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cnt % 4 == 0)
+            {
+                textBox1.Text = "←←←";
+                button1.Text = "→→→";
+            }
+            else if(cnt % 4 == 1)
+            {
+                textBox1.Text = "↑↑↑";
+                button1.Text = "↓↓↓";
+            }
+            else if (cnt % 4 == 2)
+            {
+                textBox1.Text = "→→→";
+                button1.Text = "←←←";
+            }
+            else
+            {
+                textBox1.Text = "↓↓↓";
+                button1.Text = "↑↑↑";
+            }
+            cnt++;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般的な情報は、次の方法で制御されます
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("FlexOrder_Prototype")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FlexOrder_Prototype")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("a55c8851-6f08-43ca-b3d1-e63797847191")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
