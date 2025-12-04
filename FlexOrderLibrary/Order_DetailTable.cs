ac2b10f1067c7c045db31ce4f0ff10401b880fcc
24jn0143 <24jn0143@322-PC03>
2025-12-04 10:19:48 +0900

    c

--- FlexOrderLibrary/Order_DetailTable.cs
+++ FlexOrderLibrary/Order_DetailTable.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +15,44 @@ namespace FlexOrderLibrary
             //order_seat 座席番号をInsertしない
             //いったん 
             int ret = 0;
+            int newOrderId = 0;
 
+            string connectionString = Properties.Settings.Default.DBConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = @"
+                                INSERT INTO Order
+                                    (order_date, is_takeout)
+                                VALUES
+                                    (@order_date, is_takeout);
+                                SELECT CAST(SCOPE_IDENTITY() AS int);";
 
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@order_date", neworder.order_date);
+                command.Parameters.AddWithValue("@is_takeout", neworder.is_takeout);
+                connection.Open();
+                newOrderId = (int)command.ExecuteScalar();
+                Console.WriteLine("Order に 1 件追加しました。新ID: " + newOrderId);
+            }
+            ret = 0;
+            foreach (OrderDetail detail in neworder.orderdetaillist) 
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string sql = @"INSERT INTO OrderDetail VALUES(@order_id, @goods_id, @price, @quantity, @is_provided)";
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    command.Parameters.AddWithValue("@order_id", newOrderId);
+                    command.Parameters.AddWithValue("@goods_id", detail.goods_id);
+                    command.Parameters.AddWithValue("@price", detail.price);
+                    command.Parameters.AddWithValue("@quantity", detail.quantity);
+                    command.Parameters.AddWithValue("@is_provided", detail.is_provided);
+                    connection.Open();
+                    ret += command.ExecuteNonQuery();
+                }
+            }
+            Console.WriteLine("OrderDetailに" + ret + "件追加しました");
 
-
-
-            return ret;
+            return newOrderId;
         }
     }
 }
