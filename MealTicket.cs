69e88f9421635c337ad615b4724489f6aa247675
24jn0128 <24jn0128@jec.ac.jp>
2025-12-22 13:35:04 +0900

    領収書　印刷

--- MealTicket.cs
+++ MealTicket.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlexOrderLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,32 @@ namespace FlexOrder
 {
     public partial class MealTicket : UserControl
     {
-        public MealTicket()
+        public MealTicket(Order order, OrderDetail detail)
         {
             InitializeComponent();
+            // 注文データを各ラベルに反映
+            lblDate.Text = DateTime.Now.ToString("yyyy/MM/dd");
+            lblTimeA.Text = DateTime.Now.ToString("HH/mm");
+            lblTimeB.Text = lblTimeA.Text;
+            lblGoodsNameA.Text = detail.goods_name;
+            lblGoodsNameB.Text = lblGoodsNameA.Text;
+            if (order.is_takeout==false)
+            {
+                lblIsTakeout.Text = "店内";
+            }
+            else
+            {
+                lblIsTakeout.Text = "持帰";
+            }
+            lblPriceA.Text = detail.price.ToString();
+            lblPriceB.Text = lblPriceA.Text;
+            
         }
 
         private void MealTicket_Load(object sender, EventArgs e)
         {
-
+            
         }
+        
     }
 }
