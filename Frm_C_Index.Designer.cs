5ae015414ac8a5fa89fa86a0acf28af9adc38c0a
24jn0118-Shi <24jn0118@jec.ac.jp>
2026-01-08 11:08:36 +0900

    Visual Fix

--- Frm_C_Index.Designer.cs
+++ Frm_C_Index.Designer.cs
@@ -33,8 +33,8 @@
             this.btnChinese = new System.Windows.Forms.Button();
             this.btnEnglish = new System.Windows.Forms.Button();
             this.btnJapanese = new System.Windows.Forms.Button();
-            this.btnTakeout = new System.Windows.Forms.Button();
-            this.btnDinein = new System.Windows.Forms.Button();
+            this.btnTakeout = new FlexOrder.RoundButton();
+            this.btnDinein = new FlexOrder.RoundButton();
             this.lblWelcome = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -68,16 +68,34 @@
             // 
             // btnTakeout
             // 
+            this.btnTakeout.BackColor = System.Drawing.Color.White;
+            this.btnTakeout.BorderColor = System.Drawing.Color.White;
+            this.btnTakeout.CornerRadius = 14;
+            this.btnTakeout.FlatAppearance.BorderSize = 0;
             resources.ApplyResources(this.btnTakeout, "btnTakeout");
+            this.btnTakeout.ForeColor = System.Drawing.Color.White;
+            this.btnTakeout.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(101)))), ((int)(((byte)(192)))));
             this.btnTakeout.Name = "btnTakeout";
-            this.btnTakeout.UseVisualStyleBackColor = true;
+            this.btnTakeout.NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(118)))), ((int)(((byte)(210)))));
+            this.btnTakeout.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(71)))), ((int)(((byte)(161)))));
+            this.btnTakeout.TabStop = false;
+            this.btnTakeout.UseVisualStyleBackColor = false;
             this.btnTakeout.Click += new System.EventHandler(this.btnTakeout_Click);
             // 
             // btnDinein
             // 
+            this.btnDinein.BackColor = System.Drawing.Color.Transparent;
+            this.btnDinein.BorderColor = System.Drawing.Color.White;
+            this.btnDinein.CornerRadius = 14;
+            this.btnDinein.FlatAppearance.BorderSize = 0;
             resources.ApplyResources(this.btnDinein, "btnDinein");
+            this.btnDinein.ForeColor = System.Drawing.Color.White;
+            this.btnDinein.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(47)))), ((int)(((byte)(47)))));
             this.btnDinein.Name = "btnDinein";
-            this.btnDinein.UseVisualStyleBackColor = true;
+            this.btnDinein.NormalColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(57)))), ((int)(((byte)(53)))));
+            this.btnDinein.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnDinein.TabStop = false;
+            this.btnDinein.UseVisualStyleBackColor = false;
             this.btnDinein.Click += new System.EventHandler(this.btnDinein_Click);
             // 
             // lblWelcome
@@ -110,8 +128,8 @@
         private System.Windows.Forms.Button btnChinese;
         private System.Windows.Forms.Button btnEnglish;
         private System.Windows.Forms.Button btnJapanese;
-        private System.Windows.Forms.Button btnTakeout;
-        private System.Windows.Forms.Button btnDinein;
+        private RoundButton btnTakeout;
+        private RoundButton btnDinein;
         private System.Windows.Forms.Label lblWelcome;
     }
 }
